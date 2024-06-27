75fdb8a5e437e5d837f3b53707dae100aa77cdab
narek <narek.arakelyan@outlook.be>
2024-06-27 23:44:25 +0200

    Update homescreen

--- dashboard_utils.py
+++ dashboard_utils.py
@@ -8,11 +8,6 @@ from constants import (
     sample_transactions_path,
     data_structure_path,
     sample_config_path,
-    home_title,
-    home_introduction,
-    home_privacy,
-    home_transactions_info,
-    home_config_info,
 )
 import streamlit_shadcn_ui as ui
 
@@ -92,22 +87,108 @@ def df_to_excel(df):
 
 
 def display_home():
+    example_transactions_data = df_to_excel(pd.read_excel(sample_transactions_path))
+    data_structure = pd.read_excel(data_structure_path)
+    with open(
+        sample_config_path,
+        "r",
+    ) as file:
+        yaml_data = file.read()
+
+    margins_css = """
+        <style>
+            .main > div {
+                padding-left: 10%;
+                padding-right: 10%;
+            }
+        </style>
+    """
+
+    st.markdown(margins_css, unsafe_allow_html=True)
+
     st.markdown(
-        f"""# {home_title} <span style=color:#2E9BF5><font size=5>Beta</font></span>""",
+        """
+        <style>
+        .custom-container {
+            margin-left: 0%; /* Adjust margin as needed */
+            margin-right: 0%; /* Adjust margin as needed */
+        }
+        .main-header {
+            font-size: 42px;
+            font-weight: bold;
+            text-align: center;
+            margin-bottom: 0px;
+        }
+        .sub-header {
+            font-size: 24px;
+            text-align: center;
+            color: #2E9BF5;
+        }
+        .intro-text {
+            font-size: 18px;
+            margin-bottom: 5px;
+            margin-top: 20px;
+        }
+        .section-header {
+            font-size: 28px;
+            font-weight: bold;
+            margin-top: 30px;
+            margin-bottom: 10px;
+        }
+        .feature-list, .custom-list {
+            font-size: 18px;
+        }
+        .feature-list li, .custom-list li {
+            margin-bottom: 5px;
+        }
+        .subsection-header {
+            font-size: 22px;
+            font-weight: bold;
+            margin-top: 15px;
+            margin-bottom: 15px;
+        }
+        </style>
+        """,
         unsafe_allow_html=True,
     )
-    st.markdown("""\n""")
-    st.markdown("#### Greetings")
-    st.write(home_introduction)
-    st.markdown("#### Privacy")
-    st.write(home_privacy)
-    st.markdown("#### Requirements")
-    st.markdown("###### Transactions data")
-    st.info(home_transactions_info, icon="ℹ️")
-    example_transactions_data = pd.read_excel(sample_transactions_path)
 
-    # Convert DataFrame to Excel
-    example_transactions_data = df_to_excel(example_transactions_data)
+    st.markdown(
+        """
+        <div class="main-header">Personal Finance Dashboard:</div>
+        <div class="sub-header">Take Control of Your Finances</div>
+        <div class="intro-text">Welcome to a Streamlit-based personal finance dashboard. This intuitive dashboard is designed to give you a visual representation of your finances over time, empowering you to make informed decisions and achieve your financial goals.</div>
+        <div class="section-header">What can you see here?</div>
+        <ul class="feature-list">
+            <li><strong>Track your income and expenses</strong> 📊: See exactly where your money comes from and goes. Easy-to-read visualizations break down your income streams and spending habits, helping you identify areas for potential savings or growth. Gain a comprehensive understanding of your financial patterns to make informed decisions about budgeting and resource allocation.</li>
+            <li><strong>Monitor your cash flow</strong> 💸: Stay on top of your incoming and outgoing funds. This dashboard provides clear insight into your current financial liquidity, allowing you to plan for upcoming expenses and avoid potential shortfalls. Anticipate cash crunches and optimize your spending timing to maintain a healthy financial balance.</li>
+            <li><strong>View your financial progress</strong> 📈: Charts and graphs track your progress towards your financial goals over time. Whether you're saving for a dream vacation or planning for retirement, this dashboard keeps you motivated and on track. Visualize your long-term financial journey and adjust your strategies based on real-time performance data.</li>
+        </ul>
+        <div class="section-header">Dashboard Features</div>
+        <ul class="feature-list">
+            <li><strong>Account Balance Overview:</strong> Current balances with a total sum for quick reference.</li>
+            <li><strong>Time Series Analysis:</strong> Balance trends over time for each account.</li>
+            <li><strong>Income vs. Expenses:</strong> Weekly, monthly or daily comparison of incoming and outgoing funds, helping you visualize cash flow patterns.</li>
+            <li><strong>Expense Categorization:</strong> Breakdown of expenses by category, enabling detailed spending analysis.</li>
+            <li><strong>Income Sources:</strong> Distribution of income sources, illustrated in a pie chart.</li>
+        </ul>
+        <div class="section-header">Customization Options</div>
+        <ul class="custom-list">
+            <li><strong>Date Range Selection:</strong> Focus on specific time periods using the date picker.</li>
+            <li><strong>Data Granularity:</strong> Toggle between Weekly, Daily, and Monthly views for different perspectives.</li>
+            <li><strong>Category Filtering:</strong> Select 'Category' or 'Subcategory' to adjust the level of expense detail shown.</li>
+            <li><strong>Account Selection:</strong> Use colored toggles to show/hide specific accounts in the visualizations.</li>
+        </ul>
+        <div class="section-header">Privacy</div>
+        <div class="intro-text">This application is hosted on Streamlit Cloud. Terms and services of Streamlit Cloud therefore apply.</div>
+        <div class="section-header">Requirements</div>
+        <div class="subsection-header">Transactions data</div>
+        """,
+        unsafe_allow_html=True,
+    )
+    st.info(
+        "You will need to provide an excel file (.xlsx) in the sidebar structured like this:",
+        icon="ℹ️",
+    )
 
     # Create a download button
     st.download_button(
@@ -116,18 +197,15 @@ def display_home():
         file_name="sample_transactions_file.xlsx",
         mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
     )
-    df = pd.read_excel(data_structure_path)
-    st.dataframe(df)
-    st.markdown("###### Configuration file")
-    st.info(home_config_info, icon="ℹ️")
-
-    # Streamlit app
-    # Read the YAML file
-    with open(
-        sample_config_path,
-        "r",
-    ) as file:
-        yaml_data = file.read()
+    st.dataframe(data_structure)
+    st.markdown(
+        '<div class="subsection-header">Configuration file</div>',
+        unsafe_allow_html=True,
+    )
+    st.info(
+        "You can optionally provide a configuration file (.yml) in the sidebar structured like this:",
+        icon="ℹ️",
+    )
 
     # Create a download button
     st.download_button(
@@ -140,16 +218,13 @@ def display_home():
     st.code(yaml_data, language="yml")
 
     st.markdown(
-        f"""
-        ## Frequently Asked Questions
-        """,
+        '<div class="section-header">Frequently Asked Questions</div>',
         unsafe_allow_html=True,
     )
-
     with st.expander("**How do I get my transactions?**"):
         st.markdown(
             """
-        Some bank offer a download of your transactions in a `CSV` or `Excel` format. For other types of transaction,
+        Some bank offer a download of your transactions in a `CSV` or `Excel` format. For other types of transactions,
         you can manually add them to the file (e.g. cash transactions).
         """
         )
@@ -183,11 +258,11 @@ def display_home():
         st.markdown(
             """
         I suggest having a few special categories:
-        -- TRANSFERS: This is used to cancel out transfers between your accounts.
-        -- UNKNOWN: This is used for transactions that you cannot classify.
-        -- STARTING_BALANCE: This is used to set the starting balance of your accounts if you want to start tracking from a certain date.
+        * __TRANSFERS__: This is used to cancel out transfers between your accounts.
+        * __UNKNOWN__: This is used for transactions that you cannot classify.
+        * __STARTING_BALANCE__: This is used to set the starting balance of your accounts if you want to start tracking from a certain date.
 
-        Additionaly, you should specify a category named INCOME for your income, this is required to generate the pipelot.
+        Additionaly, you should specify a category named __INCOME__ for your income, this is required to generate the pipelot.
         You can change the category name in the configuration file.
 
         Aside from these categories, you can add as many categories or subcategories as you want.
