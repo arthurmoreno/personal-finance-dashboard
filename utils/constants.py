13cee17269784b4207853e85a57f7520b0f22df0
narek <narek.arakelyan@outlook.be>
2024-07-01 23:09:34 +0200

    redo file

---
+++ utils/constants.py
@@ -0,0 +1,24 @@
+amount_col = "AMOUNT"
+date_col = "DATE"
+source_col = "SOURCE"
+type_col = "TYPE"
+time_frame_mapping = {
+    "Weekly": "YEAR_WEEK",
+    "Daily": "DATE",
+    "Monthly": "YEAR_MONTH",
+}
+category_col_mapping = {
+    "Category": "CATEGORY",
+    "Subcategory": "SUBCATEGORY",
+}
+
+default_dashboard_config_path = "example_resources/categorized/default_dashboard.yml"
+categorized_data_structure_path = "example_resources/categorized/data_structure.xlsx"
+example_categorized_transactions_path = (
+    "example_resources/categorized/transactions.xlsx"
+)
+exaple_dashboard_config_path = "example_resources/categorized/dashboard.yml"
+
+example_transactions_path = "example_resources/raw/transactions.xlsx"
+data_structure_path = "example_resources/raw/data_structure.xlsx"
+example_config_path = "example_resources/raw/categories_mapping.yml"
